34d48eb2002c3d8d589b60067de169e5b66ddcd8
Thom Badings <thom.badings@ru.nl>
2022-09-21 17:39:38 +0200

    updated benchmark run files

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
-# Define number of iterations (i)
-i=10
+# Shuttle benchmark against SReachTools
+python3 RunFile.py --model shuttle --noise_samples 1600 --confidence 0.01 --prism_java_memory 8 --monte_carlo_iter 100 --x_init '[-0.75, -0.85, 0.005, 0.005]' --plot
 #
-# Longitudinal drone dynamics benchmark
-echo -e "++++++++ RUN DRONE BENCHMARK ++++++++\n";
-python3 RunFile.py --model 'drone' --prism_java_memory 8 --iterations $i;
-python3 RunFile.py --model 'drone' --prism_java_memory 8 --drone_par_uncertainty --iterations $i;
-python3 RunFile.py --model 'drone' --prism_java_memory 8 --drone_spring --iterations $i;
-python3 RunFile.py --model 'drone' --prism_java_memory 8 --drone_spring --drone_par_uncertainty --iterations $i;
+# 2D UAV benchmark
+python3 RunFile.py --model UAV --UAV_dim 2 --noise_samples 3200 --confidence 0.01 --prism_java_memory 8 --plot
 #
-# Building temperature control problem, without epistemic uncertainty
-echo -e "++++++++ RUN TEMPERATURE CONTROL BENCHMARK (NO EPISTEMIC UNCERTAINTY) ++++++++\n";
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_partition '[15,25]' --bld_target_size '[[-.2, .2], [-.5, .5]]' --iterations $i;
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_partition '[25,35]' --bld_target_size '[[-.1, .1], [-.3, .3]]' --iterations $i;
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_partition '[35,45]' --bld_target_size '[[-.1, .1], [-.3, .3]]' --iterations $i;
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_partition '[50,70]' --bld_target_size '[[-.05, .05], [-.15, .15]]' --iterations $i;
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_partition '[70,100]' --bld_target_size '[[-.05, .05], [-.15, .15]]' --iterations $i;
+# 3D UAV benchmark
+python3 RunFile.py --model UAV --UAV_dim 3 --noise_samples 6400 --noise_factor 0.1 --nongaussian_noise --monte_carlo_iter 1000 --x_init '[-14,0,6,0,-2,0]' --plot
+python3 RunFile.py --model UAV --UAV_dim 3 --noise_samples 6400 --noise_factor 1 --nongaussian_noise --monte_carlo_iter 1000 --x_init '[-14,0,6,0,-2,0]' --plot
 #
-# Building temperature control problem, with epistemic uncertainty
-echo -e "++++++++ RUN TEMPERATURE CONTROL BENCHMARK (WITH EPISTEMIC UNCERTAINTY) ++++++++\n";
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_control_error --bld_partition '[15,25]' --bld_target_size '[[-.2, .2], [-.5, .5]]' --iterations $i;
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_control_error --bld_partition '[25,35]' --bld_target_size '[[-.1, .1], [-.3, .3]]' --iterations $i;
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_control_error --bld_partition '[35,45]' --bld_target_size '[[-.1, .1], [-.3, .3]]' --iterations $i;
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_control_error --bld_partition '[50,70]' --bld_target_size '[[-.05, .05], [-.15, .15]]' --iterations $i;
-python3 RunFile.py --model 'building_temp' --prism_java_memory 8 --bld_control_error --bld_partition '[70,100]' --bld_target_size '[[-.05, .05], [-.15, .15]]' --iterations $i;
+# 2D Building temperature control
+python3 RunFile.py --model building_2room --noise_samples 3200 --confidence 0.01 --plot
 #
-# Anaesthesia delivery problem
-echo -e "++++++++ RUN ANAESTHESIA DELIVERY BENCHMARK ++++++++\n";
-python3 RunFile.py --model 'anaesthesia_delivery' --prism_java_memory 32 --drug_partition [25,40,40];
+# 1D Building temperature control
+python3 RunFile.py --model building_1room --noise_samples 3200 --confidence 0.01 --plot
+#
+# Full spacecraft
+python3 RunFile.py --model spacecraft --noise_samples 3200 --confidence 7.86e-9 --prism_java_memory 64 --monte_carlo_iter 1000 --x_init '[0.8, 16, 0, 0, 0, 0]' --plot
+python3 RunFile.py --model spacecraft --noise_samples 3200 --confidence 7.86e-9 --prism_java_memory 64 --monte_carlo_iter 1000 --x_init '[0.8, 16, 0, 0, 0, 0]' --improved_synthesis --plot
+python3 RunFile.py --model spacecraft --noise_samples 20000 --confidence 7.86e-9 --prism_java_memory 64 --monte_carlo_iter 1000 --x_init '[0.8, 16, 0, 0, 0, 0]' --plot
+python3 RunFile.py --model spacecraft --noise_samples 20000 --confidence 7.86e-9 --prism_java_memory 64 --monte_carlo_iter 1000 --x_init '[0.8, 16, 0, 0, 0, 0]' --improved_synthesis --plot
+#
+# 2D spacecraft
+python3 RunFile.py --model spacecraft_2D --noise_samples 3200 --confidence 0.01 --prism_java_memory 8 --monte_carlo_iter 1000 --x_init '[1.2, 19.9, 0, 0]' --plot
